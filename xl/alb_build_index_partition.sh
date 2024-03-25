fc0a943dcb8d9d69592613558a92e154db246758
Tom Sherborne <tr.sherborne@gmail.com>
2024-03-25 14:55:09 +0000

    Debug embedder

--- xl/alb_build_index_partition.sh
+++ xl/alb_build_index_partition.sh
@@ -21,30 +21,33 @@ export EXP_CONFIG="$(cut -d ";" -f1 <<< "${LINE}")"
 ############################
 # Job starts from here
 ############################
-export LINE="$(sed \"${SLURM_ARRAY_TASK_ID}q;d\" ${1})"
+
+JOBID=${SLURM_ARRAY_JOB_ID}
+TASKID=${SLURM_ARRAY_TASK_ID}
+export LINE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" "$1")
 EMBED_JOB_N="$(cut -d ";" -f1 <<< "${LINE}")"
 EMBED_JOB_TOTAL="$(cut -d ";" -f2 <<< "${LINE}")"
 echo "Embedding partition ${EMBED_JOB_N} of ${EMBED_JOB_TOTAL}"
 
-# export PROJECT_HOME=$(git rev-parse --show-toplevel)
-# cd ${PROJECT_HOME}
-# MODEL_NAME="google/mt5-base"
-# FAISS_NAME="PAQ_mt5_base_sq8"
-# QAS_TO_EMBED=${PROJECT_HOME}/data/paq/TQA_TRAIN_NQ_TRAIN_PAQ/tqa-train-nq-train-PAQ.jsonl
-# EMBED_OUTPUT_DIR=${PROJECT_HOME}/data/embeddings/${FAISS_NAME}
-# FAISS_OUTPUT_DIR=${PROJECT_HOME}/data/indices
-# BSZ=1024
-# mkdir -p ${EMBED_OUTPUT_DIR}
+export PROJECT_HOME=$(git rev-parse --show-toplevel)
+cd ${PROJECT_HOME}
+MODEL_NAME="google/mt5-base"
+FAISS_NAME="PAQ_mt5_base_sq8"
+QAS_TO_EMBED=${PROJECT_HOME}/data/paq/TQA_TRAIN_NQ_TRAIN_PAQ/tqa-train-nq-train-PAQ.jsonl
+EMBED_OUTPUT_DIR=${PROJECT_HOME}/data/embeddings/${FAISS_NAME}
+FAISS_OUTPUT_DIR=${PROJECT_HOME}/data/indices
+BSZ=1024
+mkdir -p ${EMBED_OUTPUT_DIR}
 
-# python -m paq.retrievers.embed_partition \
-#     --model_name_or_path ${MODEL_NAME} \
-#     --qas_to_embed ${QAS_TO_EMBED} \
-#     --output_dir ${EMBED_OUTPUT_DIR} \
-#     --batch_size ${BSZ} \
-#     --verbose \
-#     --memory_friendly_parsing \
-#     --fp16 \
-#     --n_jobs ${EMBED_JOB_TOTAL} \
-#     --embed_job_n ${EMBED_JOB_N}
+python -m paq.retrievers.embed_partition \
+    --model_name_or_path ${MODEL_NAME} \
+    --qas_to_embed ${QAS_TO_EMBED} \
+    --output_dir ${EMBED_OUTPUT_DIR} \
+    --batch_size ${BSZ} \
+    --verbose \
+    --memory_friendly_parsing \
+    --fp16 \
+    --n_jobs ${EMBED_JOB_TOTAL} \
+    --embed_job_n ${EMBED_JOB_N}
 
 echo "Completed Index generation"
